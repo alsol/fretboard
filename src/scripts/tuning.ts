a1456e9cd0ebe9c81c8cff79bba5506b8f25af66
Alexander Solovyov <xerox.vrn@gmail.com>
2021-02-18 23:33:21 +0300

    add base types for instruments/tunings

---
+++ src/scripts/tuning.ts
@@ -0,0 +1,46 @@
+import {GUITAR_TUNINGS} from '@moonwave99/fretboard.js';
+
+export interface Tuning {
+    title: string,
+    strings: string[]
+}
+
+export interface Instrument {
+    title: string,
+    tunings: Tuning[]
+}
+
+const bass: Instrument = {
+    title: "Bass",
+    tunings: [
+        {title: 'standard', strings: ['E1', 'A1', 'D2', 'G2']}
+    ]
+}
+
+const uke: Instrument = {
+    title: 'Ukulele',
+    tunings: [
+        {title: 'standard', strings: ['G4','C4','E4','A4']}
+    ]
+}
+
+const guitar: Instrument = {
+    title: "Guitar",
+    tunings: [
+        {title: 'standard', strings: GUITAR_TUNINGS.default},
+        {title: 'drop D', strings: GUITAR_TUNINGS.dropD},
+        {title: 'drop C', strings: ['C2', 'G2', 'C3', 'F3', 'A3', 'D4']}
+    ]
+}
+
+const guitar7: Instrument = {
+    title: "Guitar 7s",
+    tunings: [
+        {title: 'standard', strings: ['B2', ...GUITAR_TUNINGS.default]},
+        {title: 'drop A', strings: ['A2', ...GUITAR_TUNINGS.default]},
+    ]
+}
+
+export const instruments: Instrument[] = [
+    guitar, guitar7, bass, uke
+]
