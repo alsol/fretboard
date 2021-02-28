f04e0a872dc2d4c88e1ecb645bfb7b4ab43ebada
Alexander Solovyov <xerox.vrn@gmail.com>
2021-02-28 11:43:24 +0300

    change chord system type

--- src/scripts/chords.ts
+++ src/scripts/chords.ts
@@ -4,43 +4,43 @@ import {guitar, Instrument} from "./tuning";
 
 export interface Chord {
     root: string,
-    pattern: string,
-    instrument?: Instrument
+    pattern: string
 }
 
-export type ChordSystem = Chord[]
+export type ChordSystem = {
+    chords: Chord[],
+    instrument?: Instrument
+}
 
 const C: Chord = {
     root: 'C',
     pattern: 'xR35R3',
-    instrument: guitar
 }
 
 const A: Chord = {
     root: 'A',
     pattern: 'xR5R35',
-    instrument: guitar
 }
 
 const G: Chord = {
     root: 'G',
     pattern: 'xx5R5R',
-    instrument: guitar
 }
 
 const E: Chord = {
     root: 'E',
     pattern: 'R5R35R',
-    instrument: guitar
 }
 
 const D: Chord = {
     root: 'D',
     pattern: 'xxR5R3',
-    instrument: guitar
 }
 
-export const CAGED: ChordSystem = [C, A, G, E, D]
+export const CAGED: ChordSystem = {
+    chords: [C, A, G, E, D],
+    instrument: guitar
+}
 
 export function renderChord(chord: Chord, system: FretboardSystem): string {
     const rootScale: Position[] = system.getScale({root: chord.root, type: 'major'})
